3095e9d4c883b52def2b1c53c81fdfbcd3a7a3d7
Macrobull <MacrobullG@Gmail.com>
2014-06-02 15:38:26 +0800

    new framework, uart cleared

--- include/qprintf.h
+++
@@ -1,7 +0,0 @@
-#ifndef __QPRINTF_H
-#define __QPRINTF_H
-
-extern int sprintf(char *buf, const char *fmt, ...);
-extern int qprintf(USCI_UART_info *uart, const char *fmt, ...);
-
-#endif
--- include/seg_disp/8seg.h
+++
@@ -1,9 +0,0 @@
-
-#ifndef __8SEG_H
-#define __8SEG_H
-
-#include "seg_disp/7seg.h"
-
-#define	DOT	BIT7
-
-#endif
